69201c1bffd223aec7d45a8eaf36b31a47e205fa
Your Name <384211933@qq.com>
2019-12-08 18:06:13 +0800

    2th

---
+++ build/precache-manifest.6cc54f9c12f43435bb8eaec24b3613da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf937c9a2b91a2c98dbd103f9187765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d2dc2ba9f396b22b0f",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "b0ef29cd4b1e1b4eeef3",
+    "url": "/static/css/main.336ba782.chunk.css"
+  },
+  {
+    "revision": "74d2dc2ba9f396b22b0f",
+    "url": "/static/js/2.ba28bd2f.chunk.js"
+  },
+  {
+    "revision": "b0ef29cd4b1e1b4eeef3",
+    "url": "/static/js/main.5fb14efb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/api/shop.js
+++ src/api/shop.js
@@ -4,6 +4,6 @@
 import axios from 'axios'
 
 export default {
-  getProducts: () => axios.get('/products.json'),
+  getProducts: () => axios.get('products.json'),
   buyProducts: (payload) => new Promise((resolve, reject) => setTimeout(() => resolve(payload), 2000)) //只是为了模拟axios.post的效果
 }
