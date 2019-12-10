e25acc0b8a2f3485048bc9bfd0c0f1044aaac089
Your Name <384211933@qq.com>
2019-12-10 16:17:39 +0800

    Updates

---
+++ precache-manifest.17792a8ace627589ebb0295dd938845d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b508b2cb0aa1d39f92d2a30cab809d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c29f66c3e6f395505c85",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "be04189d745e7bc70990",
+    "url": "/static/css/main.4a5b5cc3.chunk.css"
+  },
+  {
+    "revision": "c29f66c3e6f395505c85",
+    "url": "/static/js/2.44549282.chunk.js"
+  },
+  {
+    "revision": "be04189d745e7bc70990",
+    "url": "/static/js/main.725afca3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
